46751b13a72dd4c5cdae89838a9914faf644c8ce
Ariel Agolli <arielagolli7@gmail.com>
2022-10-22 18:08:50 +0200

    Added navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
+    <Navbar/>
   </header>
 
   <RouterView />
