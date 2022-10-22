9adb1f84b7bebdc8034946509dae828d56bfccd1
Ariel Agolli <arielagolli7@gmail.com>
2022-10-23 00:12:46 +0200

    Fixed login post

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,4 @@
 <template>
-  <!--
-      This example requires updating your template:
-  
-      ```
-      <html class="h-full bg-gray-50">
-      <body class="h-full">
-      ```
-    -->
   <div
     class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
   >
@@ -30,18 +22,19 @@
           >
         </p>
       </div>
-      <form class="mt-8 space-y-6" action="#" method="POST">
+      <form class="mt-8 space-y-6">
         <input type="hidden" name="remember" value="true" />
         <div class="-space-y-px rounded-md shadow-sm">
           <div>
-            <label for="email-address" class="sr-only">Email address</label>
+            <label for="username" class="sr-only">Username</label>
             <input
-              id="email-address"
-              name="email"
-              type="email"
-              autocomplete="email"
+              id="username"
+              name="username"
+              type="text"
+              autocomplete="username"
               class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-              placeholder="Email address"
+              placeholder="Username"
+              v-model="username"
             />
           </div>
           <div>
@@ -53,6 +46,7 @@
               autocomplete="current-password"
               class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
               placeholder="Password"
+              v-model="password"
             />
           </div>
         </div>
@@ -81,7 +75,7 @@
 
         <div>
           <button
-            type="submit"
+            @click="submit($event)"
             class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >
             <span class="absolute inset-y-0 left-0 flex items-center pl-3">
@@ -103,7 +97,37 @@ import { LockClosedIcon } from "@heroicons/vue/20/solid";
 </script>
 
 <script lang="ts">
+import axios from "axios";
+
 export default {
   name: "LoginComponent",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    async login(data: any) {
+      await axios
+        .post("http://localhost:8080/api/user/login", data, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then((res) => {
+          console.log(res.data.data);
+        });
+    },
+    submit(e: any) {
+      let data = {
+        username: this.username,
+        password: this.password,
+      };
+      this.login(data);
+      e.preventDefault();
+      this.$router.push("/");
+    },
+  },
 };
 </script>
